639d816b05b1a307fb61cb7c411ed95b3e86b8e7
abhises <abhisespoudyal@gmail.com>
2025-04-30 21:26:17 +0545

    cors added

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express from "express";
+import cors from "cors";
 import postRoutes from "./routes/postRoutes";
 
 const app = express();
+
+// Enable all CORS requests (open access)
+app.use(cors());
+
+// For JSON body parsing
 app.use(express.json());
 
+// Routes
 app.use("/posts", postRoutes);
 
+// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
